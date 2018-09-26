ebf12f8e2ccf5ef558ce403b2939da6b8b49ce95
Matt Grainger <grainger.pdx@gmail.com>
2018-09-26 09:21:10 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+%matplotlib inline
+import seaborn as sns
+
+df = pd.read_csv('Pokemon.csv',encoding = "ISO-8859-1", index_col=0)
+
+#df.head()
+#sns.lmplot(x="Attack", y="Defense", data=df, fit_reg = False, hue = "Stage")
+
+#plt.ylim(0, None)
+#plt.xlim(0, None)
+
+2
+3
+4
+5
+	
+# Pre-format DataFrame
+#stats_df = df.drop(['Total', 'Stage', 'Legendary'], axis=1)
+ 
+# New boxplot using stats_df
+#sns.boxplot(data=stats_df)
+
+# Set theme
+sns.set_style('whitegrid')
+sns.set(rc={'figure.figsize':(15,5)}) 
+# Violin plot
+
+
+pkmn_type_colors = ['#78C850',  # Grass
+                    '#F08030',  # Fire
+                    '#6890F0',  # Water
+                    '#A8B820',  # Bug
+                    '#A8A878',  # Normal
+                    '#A040A0',  # Poison
+                    '#F8D030',  # Electric
+                    '#E0C068',  # Ground
+                    '#EE99AC',  # Fairy
+                    '#C03028',  # Fighting
+                    '#F85888',  # Psychic
+                    '#B8A038',  # Rock
+                    '#705898',  # Ghost
+                    '#98D8D8',  # Ice
+                    '#7038F8',  # Dragon
+                   ]
+
+
+sns.violinplot(x='Type 1', y='Attack', inner = None, data=df)
+# Violin plot with Pokemon color palette
+sns.violinplot(x='Type 1', y='Attack', data=df, pallette = inner=None ) # Set color palette
+
+sns.swarmplot(x='Type 1', y='Attack', data=df, 
+              palette=pkmn_type_colors)
